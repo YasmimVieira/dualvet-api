23c2157addf5be7b185eb9a8ce89ba176e170778
YasmimVieira <vieiray937@gmail.com>
2022-09-20 21:58:28 -0300

    fix: add header access

--- src/app.js
+++ src/app.js
@@ -9,7 +9,15 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(express.json())
+app.use(function(res, next) { 
+    res.setHeader('Access-Control-Allow-Origin', process.env.PORT);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+    express.json()
+})
 
 routes(app);
 

1aa3576d250c2381762ed65b2043376261354412
YasmimVieira <vieiray937@gmail.com>
2022-09-20 22:13:29 -0300

    fix: headers alow access

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 db.on("erro", console.log.bind(console, 'Erro de conexao'))
 db.once("open", () => {
@@ -9,15 +10,10 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(function(res, next) { 
-    res.setHeader('Access-Control-Allow-Origin', process.env.PORT);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
-    express.json()
-})
+app.use(cors({
+    origin: "*"
+}))
+app.use(express.json())
 
 routes(app);
 

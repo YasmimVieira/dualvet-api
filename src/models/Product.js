e9b2d5c89610ed173eaad75905c58e366f308f7f
YasmimVieira <vieiray937@gmail.com>
2022-10-13 23:40:18 -0300

    fix: delete id

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        id: { type: String },
         productName: { type: String },
         productDescription: { type: String },
         productPrice: { type: String },
